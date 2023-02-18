0146b1dbecba672778e658268639c9c2e9e10603
Noah <noah@Noahs-Air.lan>
2023-02-18 17:24:15 +0100

    bugfix

--- popup.js
+++ popup.js
@@ -7,45 +7,46 @@ async function getAllTabs() {
     });
     return tabArray;
   }
-console.log("This is a popup!");
-console.log(getAllTabs());
 //promise result
 var tabs = [];
 var tabKeeper = [];
-getAllTabs().then(function (result) 
-{ 
-    console.log(result);
-    result.forEach(function (tab) {
-        tabs.push(tab);
-        var li = document.createElement("li");
-        liste.appendChild(li);
-        li.innerHTML = tab.title;
-        let clicked = false;
-        li.addEventListener("click", function () {
-            if (clicked) {
-                li.style.color = "black";
-                clicked = false;
-                tabKeeper.splice(tabKeeper.indexOf(tab), 1);
-            } else {
-                li.style.color = "red";
-                clicked = true;
-                tabKeeper.push(tab);
+
+function getAllPromise(){
+    getAllTabs().then(function (result) 
+    { 
+        console.log(result);
+        result.forEach(function (tab) {
+            tabs.push(tab);
+            var li = document.createElement("li");
+            liste.appendChild(li);
+            li.innerHTML = tab.title;
+            let clicked = false;
+            li.addEventListener("click", function () {
+                if (clicked) {
+                    li.style.color = "black";
+                    clicked = false;
+                    tabKeeper.splice(tabKeeper.indexOf(tab), 1);
+                } else {
+                    li.style.color = "red";
+                    clicked = true;
+                    tabKeeper.push(tab);
+                }
             }
-        }
-        );
-    });
-    //getid btn
-    var btn = document.getElementById("btn");
-    btn.addEventListener("click", function () {
-        tabs.forEach(function (tab) {
-            if (tabKeeper.indexOf(tab) == -1) {
-                chrome.tabs.remove(tab.id);
-            }
+            );
+        });
+        var btn = document.getElementById("btn");
+        btn.addEventListener("click", function () {
+            tabs.forEach(function (tab) {
+                if (tabKeeper.indexOf(tab) == -1) {
+                    chrome.tabs.remove(tab.id);
+                }
+            });
         });
     });
-});
+}
 
 
 
 
 
+getAllPromise();
